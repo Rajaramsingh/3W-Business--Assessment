f20398cee0c09db8cb2bfbf7b68b7c32e0c89582
Rajaram singh <rajaramsingh.cse2023@ritroorkee.com>
2025-07-21 02:10:35 +0530

    add routes nad import some functions

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables
+const {initializeUsers} = require ('./utils/helpers')
+const userRoutes = require('./routes/userRoutes');
+const historyRoutes = require('./routes/historyRoutes');
+
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
+// routes
+app.use('/users', userRoutes);
+app.use('/history', historyRoutes);
+
+
 connectDB(); // connect to database
+
+
+
+// Initialize default users
+initializeUsers();
+
+
+app.get('/test', (req, res) => {
+  res.send('Server is working!');
+});
+const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
